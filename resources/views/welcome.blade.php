c792f12b163714ec4ad4ab31093b99dddb90cc27
fann133 <irfanfann005@gmail.com>
2025-05-15 17:16:51 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-5xl text-rose-600 text-center mt-10">Hello Tailwind v4 🚀</h1>
+</body>
+</html>
